b6abc480f0e914c236ce10ed62cdef8c2012b780
Gaze-Fish-72 <vanshgoswami445@gmail.com>
2026-09-22 23:11:38 +0530

    Time: 307 ms (19.74%) | Memory: 81.5 MB (14.63%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n=s.length();
+        unordered_set<int>st;
+        int l=0;
+        int len=0;
+        for(int r=0;r<n;r++){
+            while(st.find(s[r])!=st.end()){
+                st.erase(s[l]);
+                l++;
+            }
+            st.insert(s[r]);
+            len=max(len,r-l+1);
+        }
+        return len;
+    }
+};
