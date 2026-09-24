5b0d8ee3999f0e65798de01c21765f2a30d84eea
Gaze-Fish-72 <vanshgoswami445@gmail.com>
2026-09-24 23:10:43 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (94.28%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.empty()) return "";
+        string s="";
+        int i=0;
+        while(i<strs[0].size()){
+            for(int j=0;j<strs.size();j++){
+                if(strs[j][i]!=strs[0][i]) return s;
+            }
+            s+=strs[0][i];
+            i++;
+        }
+        return s;
+    }
+};
