4df63f49fb7f45ceec1130419c12a6e2635656ec
Gaze-Fish-72 <vanshgoswami445@gmail.com>
2026-09-26 22:59:17 +0530

    Time: 208 ms (9.66%) | Memory: 122.1 MB (83.49%) - LeetSync

--- 1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.cpp
+++ 1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.cpp
@@ -2,32 +2,23 @@ class Solution {
 public:
     string evaluate(string s, vector<vector<string>>& knowledge) {
         unordered_map<string, string> mp;
-
-        for(auto &i : knowledge) {
+        for (auto& i : knowledge) {
             mp[i[0]] = i[1];
         }
-
         int i = 0;
-
-        while(i < s.length()) {
-
-            if(s[i] == '(') {
-
+        while (i < s.length()) {
+            if (s[i] == '(') {
                 int j = s.find(')', i);
-
-                string key = s.substr(i + 1, j - i - 1);
-
-                if(mp.find(key) != mp.end()) {
-                    s.replace(i, j - i + 1, mp[key]);
-                }
+                string e = s.substr(i + 1, j - i - 1);
+                if (mp.find(e) != mp.end()) {
+                    s.replace(i, j - i + 1, mp[e]);
+                } 
                 else {
                     s.replace(i, j - i + 1, "?");
                 }
             }
-
             i++;
         }
-
         return s;
     }
 };
